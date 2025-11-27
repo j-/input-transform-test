6b4e838d20a3eb74e7d9d603903d705684ae8855
j- <j@skeoh.com>
2025-11-27 15:39:46 +1100

    Add link to input event command types

--- src/make-input-transform/constants.ts
+++ src/make-input-transform/constants.ts
@@ -1,3 +1,6 @@
+// Commands defined here:
+// https://w3c.github.io/input-events/#interface-InputEvent-Attributes
+
 export const COMMAND_INSERT_FROM_DROP = 'insertFromDrop';
 export const COMMAND_INSERT_FROM_PASTE = 'insertFromPaste';
 export const COMMAND_INSERT_REPLACEMENT_TEXT = 'insertReplacementText';
